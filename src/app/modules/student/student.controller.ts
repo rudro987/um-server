73edbcd5884276a58054f6b319819d6456f7ab96
Tanvir Rudro <rudro987@gmail.com>
2024-08-11 19:37:41 +0600

    get all student and get single student routes,controller and service created

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -3,8 +3,8 @@ import { StudentServices } from "./student.service";
 
 const createStudent = async (req: Request, res: Response) => {
   try {
-    const student = req.body;
-    const result = await StudentServices.createStudentIntoDB(student);
+    const { student: studentData } = req.body;
+    const result = await StudentServices.createStudentIntoDB(studentData);
     res.status(200).json({
       success: true,
       message: "Student is created successfully",
@@ -19,6 +19,43 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+    res.status(200).json({
+      success: true,
+      message: "Students data retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: "Student fetching failed",
+      data: error,
+    });
+  }
+}
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+    res.status(200).json({
+      success: true,
+      message: "Student's data retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: "Student fetching failed",
+      data: error,
+    });
+  }
+}
+
 export const StudentControllers = {
   createStudent,
+  getAllStudents,
+  getSingleStudent
 };
