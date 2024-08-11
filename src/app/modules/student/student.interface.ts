6b75e23c70c70758f01e531c273373e9f9f6a842
Tanvir Rudro <rudro987@gmail.com>
2024-08-11 18:09:59 +0600

    student model created

--- src/app/modules/student/student.interface.ts
+++ src/app/modules/student/student.interface.ts
@@ -24,7 +24,7 @@ export type Student = {
   id: string;
   name: UserName;
   gender: "male" | "female";
-  dateOfBirth: string;
+  dateOfBirth?: string;
   email: string;
   contactNo: string;
   emergencyContactNo: string;
@@ -34,5 +34,5 @@ export type Student = {
   guardian: Guardian;
   localGuardian: LocalGuardian;
   profileImg?:string;
-  isActive: 'active' | 'inactive';
+  isActive: 'active' | 'blocked';
 };
